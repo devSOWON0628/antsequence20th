18d5cd70ccd24954af0f5d2fe920e78da8d6e7b8
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-12 14:15:33 +0900

    개미

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,52 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            
+            int count = 1;
+            int a = 0;
+            int b = 0;
+            
+            int[] realArray = new int[500];
+            int[] justarray = new int[500];
+
+            realArray[0] = 1;
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                while (realArray[a] != 0)
+                {
+                    if (realArray[a] == realArray[a + 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        justarray[b] = realArray[a];       
+                        justarray[b + 1] = count;      
+                        count = 1;                 
+                        b = b + 2;                 
+                    }
+                    a++;
+                }
+
+                Array.Copy(justarray, realArray, justarray.Length);
+                a = 0;
+                b = 0;
+
+                
+
+                if (i == 19) {
+                    Console.Write(i + 1 + "번째 : ");
+                    foreach (var arr in realArray)
+                {
+                    if (arr == 0) break;
+                    Console.Write(arr);
+                }
+                Console.WriteLine();
+            }
+                }
         }
     }
-}
+    }
+
